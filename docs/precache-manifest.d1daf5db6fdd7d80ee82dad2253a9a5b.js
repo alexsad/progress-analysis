69cd9c5aac35935bc81fd653e1e23cf3376f1226
alexsad <alex.query@gmail.com>
2020-11-26 17:31:14 -0300

    doc as homepage

---
+++ docs/precache-manifest.d1daf5db6fdd7d80ee82dad2253a9a5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e2ddfe46123d90c322aee8c97e7139",
+    "url": "/index.html"
+  },
+  {
+    "revision": "165c98d552568da8f747",
+    "url": "/static/css/main.ebf33bb0.chunk.css"
+  },
+  {
+    "revision": "99b0faf5cc7f833cc42e",
+    "url": "/static/js/2.b2f76ede.chunk.js"
+  },
+  {
+    "revision": "165c98d552568da8f747",
+    "url": "/static/js/main.e2e750ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7916d270843af9a264ea2120f3853f8",
+    "url": "/static/media/k-bigpic-min.b7916d27.jpg"
+  }
+]);
